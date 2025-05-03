eab1291b8d6d3367d2670d2433ad98870f01dc5c
Chris-Mods1 <takasession@gmail.com>
2025-05-03 11:06:10 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ const mega = require('megajs')
 
 const auth = {
     email: 'Christakaa84@gmail.com',
-    password: 'Vendredi2005',
+    password: 'Vendredi2006',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
