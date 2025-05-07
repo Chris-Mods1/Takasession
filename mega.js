82274fc9cf147dbfffc070a9a04ff8ac0cf489bc
Chris-Mods1 <takasession@gmail.com>
2025-05-08 01:02:22 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ const mega = require('megajs')
 
 const auth = {
     email: 'diorrebero84@gmail.com',
-    password: 'kokdar-dibfus-3Xoqme',
+    password: 'Vendredi2004',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
